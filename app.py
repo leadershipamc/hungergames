b75a6dbb6f39f30c8158e3ecc5a53bfd603cf0ee
Ubuntu <ubuntu@domU-12-31-39-02-7C-21.compute-1.internal>
2013-08-01 06:17:34 +0000

    Added bots

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from Player import Player
 # Bare minimum test game. See README.md for details.
 
 if __name__ == '__main__':
-    players = [Player(), Pushover(), Freeloader(), Alternator(), MaxRepHunter(), Random(.2), Random(.8)]
+    players = [Player(), Pushover(), Freeloader(), Alternator(), MaxRepHunter(), Random(.2), Random(.8), FairHunter(), BoundedHunter(0.4,0.6), AverageHunter()]
     game = Game(players)
     game.play_game()
